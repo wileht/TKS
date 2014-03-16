a3ec2fa1569ab0848d1a02961a9fe1d556e9f450
WL <wille.lehtomaki@helsinki.fi>
2014-03-16 18:10:05 +0200

    index

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+  $blib = "Blub"; 
+?><!DOCTYPE HTML>
+<html>
+<head><title><?php echo $blib; ?></title></head>
+<body>
+  <h1><a href="http://wlehtoma.users.cs.helsinki.fi/Keskustelufoorumi/esittelysivu.html"><?php echo $blib; ?></a></h1>
+</body>
+</html>
